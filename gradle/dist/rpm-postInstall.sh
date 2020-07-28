77b0112be32f1cbc1a1d82ab418ce502d0f17760
Daniel Lamando <dan@danopia.net>
2020-07-28 18:51:44 +0200

    Move rpm folder setup to postinstall

--- gradle/dist/rpm-postInstall.sh
+++ gradle/dist/rpm-postInstall.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 if [ $1 -eq 1 ] && [ -x /usr/bin/systemctl ] ; then
   # Initial installation
+
+  if [ -d /etc/wireguard ]; then
+    chown -R :kube-pet /etc/wireguard
+    chmod -R g+rw /etc/wireguard
+  fi
+
+  if ! [ -d /opt/kube-pet-node/.cache ]; then
+    mkdir /opt/kube-pet-node/.cache
+    chown -R kube-pet:kube-pet /opt/kube-pet-node/.cache
+    chmod -R 0700 /opt/kube-pet-node/.cache
+  fi
+
   /usr/bin/systemctl enable kube-podman.service >/dev/null 2>&1 || :
   /usr/bin/systemctl enable --now kube-podman.socket >/dev/null 2>&1 || :
   /usr/bin/systemctl enable --now kube-pet-node.service >/dev/null 2>&1 || :
