b01b0c4e06a8656aacf6a19e53a45aad9e9ecea6
Daniel Lamando <dan@danopia.net>
2020-10-27 19:43:42 +0100

    volumes: forgot to change the command that MATTERS

--- controllers/volumes/volume-injector.go
+++ controllers/volumes/volume-injector.go
@@ -15,7 +15,7 @@ func startArchiveExtractin(ctx context.Context, dest string) (*ArchiveExtraction
 	if os.Getuid() == 1 {
 		cmd = exec.CommandContext(ctx, "tar", "-xf", "-", "-C", dest)
 	} else {
-		cmd = exec.CommandContext(ctx, "sudo", "tar", "-xvf", "-", "-C", dest)
+		cmd = exec.CommandContext(ctx, "sudo", "tar", "-xf", "-", "-C", dest)
 	}
 
 	stdin, err := cmd.StdinPipe()
